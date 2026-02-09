41dba991307755c0aabbde7a847e6a829855c9b2
Jorgerdzz <asilgalan083@gmail.com>
2026-02-09 14:22:37 +0100

    Add project files.

---
+++ MvcCoreEF/Data/HospitalContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcCoreEF.Models;
+
+namespace MvcCoreEF.Data
+{
+    public class HospitalContext: DbContext
+    {
+        //EL CONSTRUCTOR RECIBIRA LAS OPCIONES PARA ESTE CONTEXTO
+        //La clase que recibe es DbContextOptions<Context>
+        //ESTAS options DEBEMOS ENVIARLAS A LA CLASE BASE/SUPER DEL DbContext
+        public HospitalContext(DbContextOptions<HospitalContext> options):base(options)
+        {
+
+        }
+
+        //DEBEMOS TENER UNA COLECCION POR CADA MODEL
+        //DICHA COLECCION DEBER SER DE TIPO DbSet<T>
+        public DbSet<Hospital> Hospitales { get; set; }
+        
+    }
+}
